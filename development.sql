9a1d18a32dee5427bb8dec0c674353554c18a24d
jiangxiaosong <610761341@qq.com>
2018-11-27 11:22:06 +0800

    计划项目sql上传

---
+++ development.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 192.168.0.13
+Source Server Version : 50717
+Source Host           : 192.168.0.13:3306
+Source Database       : development
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2018-11-27 09:29:42
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
